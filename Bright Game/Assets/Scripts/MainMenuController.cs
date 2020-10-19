a0875e8091cbc83bff1faab03a2e81859c6c2f88
Daria Tan <tdaria207@gmail.com>
2020-10-18 23:45:14 -0500

    Add "Bright-Level 0" Scene and Add MainMenuController to make buttons work
    Also add scenes to build settings

---
+++ Bright Game/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour {
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void PlayGame() {
+        // Loads the next scene in the build settings after this one
+        // TODO: currently set to go to Will'sSandbox, need to change to the proper game scene
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void ToOptionsMenu() {
+
+    }
+
+    public void ToMainMenu() {
+
+    }
+
+    public void ExitGame() { }
+}
