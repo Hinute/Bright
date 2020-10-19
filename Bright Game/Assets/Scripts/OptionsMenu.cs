33202465cf406f730ac87d838a636e6bda0069e0
Daria Tan <tdaria207@gmail.com>
2020-10-19 00:35:39 -0500

    (m) MainMenu: OptionsMenu: adjust objects

---
+++ Bright Game/Assets/Scripts/OptionsMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsMenu : MonoBehaviour {
+    public Slider musicVolumeSlider;
+    public Slider effectsVolumeSlider;
+
+    void Start() {
+        musicVolumeSlider.value = PlayerPrefs.GetFloat("MusicVolume", 0.75f);
+        effectsVolumeSlider.value = PlayerPrefs.GetFloat("EffectsVolume", 0.75f);
+    }
+    public void updateMusicVolume() {
+        PlayerPrefs.SetFloat("MusicVolume", musicVolumeSlider.value);
+        AudioManager.instance.musicVolumeChanged();
+    }
+
+    public void updateEffectsVolume() {
+        PlayerPrefs.SetFloat("EffectsVolume", effectsVolumeSlider.value);
+        AudioManager.instance.effectVolumeChanged();
+    }
+}
